47245b66a28dba1ef8eed352ef4a51a6c032cd50
codriv <combodriver4@yahoo.at>
2023-02-17 14:28:57 +0100

    init

---
+++ sv2022-jvjbf-zarovizsga/src/main/java/org/training360/sv2jvjbfzarovizsga/Sv2JvjbfZarovizsgaApplication.java
@@ -0,0 +1,13 @@
+package org.training360.sv2jvjbfzarovizsga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2JvjbfZarovizsgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sv2JvjbfZarovizsgaApplication.class, args);
+    }
+
+}
