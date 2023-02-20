6a3e332bd5913f71a9f4ffff78d94fb48cb9355a
codriv <combodriver4@yahoo.at>
2023-02-20 16:12:33 +0100

    removed

--- sv2022-jvjbf-zarovizsga-potvizsga/src/main/java/org/training360/sv2022jvjbfzarovizsgapotvizsga/model/HospitalWard.java
+++
@@ -1,6 +0,0 @@
-package org.training360.sv2022jvjbfzarovizsgapotvizsga.model;
-
-public enum HospitalWard {
-
-    MEDICAL, TRAUMATOLOGY, RHEUMATOLOGY, ORTHOPAEDY
-}
--- sv2022-jvjbf-zarovizsga-potvizsga/src/main/java/org/training360/sv2022jvjbfzarovizsgapotvizsga/repository/HospitalRepository.java
+++
@@ -1,8 +0,0 @@
-package org.training360.sv2022jvjbfzarovizsgapotvizsga.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.training360.sv2022jvjbfzarovizsgapotvizsga.model.Hospital;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-
-}
